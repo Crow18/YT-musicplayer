fd6bc67d66ac3dc7a6d3afe8fbb656e5959cc5ac
Crow18 <ligumacarl@gmail.com>
2020-02-23 11:34:40 +0200

    replay done

--- Play.sh
+++ Play.sh
@@ -2,8 +2,6 @@
 
 song=$1
 bool=0
-Bool=0
-re='^[0-9]+$'
 
 install-dependencies()
 {
@@ -13,38 +11,46 @@ install-dependencies()
 	sudo chmod a+rx /usr/local/bin/youtube-dl
 }
 
+
+
+SelectPlay()
+{
+	mpv "$(youtube-dl -a .YTSelect --default-search ytsearch --get-url | tail -1)"
+}
+
+ReadNPlay()
+{
+	if [ -s ".YTStreamList" ]
+		then
+			echo which track would you like to hear?
+			read num
+			echo "Loading..."
+			newselect="sed -n "$num"p .YTStreamList"
+			eval $newselect | cut -d " " -f 2- | tee .YTSelect | tr -s " " | tr -dc '[:alnum:]\n '
+			play=$(cat ".YTSelect")
+			SelectPlay
+			rm -rf .YTStreamList
+		else
+			echo "something went wrong!"
+		fi
+}
+
+
 SelectFromSearch()
 {
 	if [[ -z "$song" ]]
 	then
-	       	echo "Enter a song to search as first arg: ./Play.sh <song name>"
+	       	echo "Enter a song to search as first arg"
        	else
 		echo "Searching please wait..."
 		selection="youtube-dl --newline --get-title --default-search ytsearch5: $song"
 		eval "$selection" | awk '{printf "%d %s\n", NR, $0}' | tee .YTStreamList
-		if [ -s ".YTStreamList" ]
-		then
-			echo which track would you like to hear?
-			read num
-			
-			#if [[ $1 =~ $re ]]
-			#then
-				echo "Loading..."
-				newselect="sed -n "$num"p .YTStreamList"
-				eval $newselect | cut -d " " -f 2- | tee .YTSelect | tr -s " " | tr -dc '[:alnum:]\n '
-			#play=$(cat ".YTSelect")
-				mpv "$(youtube-dl -a .YTSelect --default-search ytsearch --audio-format mp3 --get-url | tail -1)"
-				rm -rf .YTStreamList
-				rm -rf .YTSelect
-			
-		else
-			echo "Oops! something went wrong... try checking your internet connection"
-		fi
+		ReadNPlay
 	fi
 }
 
 
-if [ $(sudo ls /usr/local/bin | grep -c "youtube-dl") -eq 0 ]
+if [ $(ls /usr/local/bin | grep -c "youtube-dl") -eq 0 ]
 then
 	let "bool++"
 elif [ $(dpkg-query -Wf='${Status}' mpv  2> /dev/null | grep -c "installed") -eq 0 ]
@@ -57,5 +63,10 @@ then
 	install-dependencies
 	SelectFromSearch $1
 else
-	SelectFromSearch $1
+	if [ $1 == "--replay" ]
+	then
+		SelectPlay
+	else
+		SelectFromSearch $1
+	fi
 fi
